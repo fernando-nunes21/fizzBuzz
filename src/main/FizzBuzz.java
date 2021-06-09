55019912d9d682226cd3fbf249020ef94c7855d2
fernando-nunes21 <luis@ubots.com.br>
2021-06-09 03:03:59 -0300

    - Test GREEN -

--- src/main/FizzBuzz.java
+++ src/main/FizzBuzz.java
@@ -2,7 +2,7 @@ package main;
 
 public class FizzBuzz implements FizzBuzzInterface {
     public String numberDividedByThree(Integer number){
-        if(number == 3){
+        if(number %3 == 0){
             return "Fizz";
         }
         else{
