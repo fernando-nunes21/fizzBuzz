efa49acaf0c8b24545b9e0f30e32574ee08326fc
fernando-nunes21 <luis@ubots.com.br>
2021-06-10 02:12:56 -0300

    - Final Version, Main run fizzBuzz 1 to 100 -

--- src/main/ApplicationMain.java
+++ src/main/ApplicationMain.java
@@ -1,7 +1,9 @@
 package main;
 
 public class ApplicationMain {
-    public static void main(String[] args) {
-
+    public static void main(String[] args){
+        for (int i=1;i<=100;i++){
+            System.out.println(new FizzBuzz().testNumber(i));
+        }
     }
 }
