00c64c8d32295dd7d61f053824fd1c69a50bdeb4
fernando-nunes21 <luis@ubots.com.br>
2021-06-10 01:23:50 -0300

    - Test RED -

--- src/tests/ApplicationTests.java
+++ src/tests/ApplicationTests.java
@@ -9,11 +9,15 @@ public class ApplicationTests extends TestCase {
         assertEquals("Fizz", new FizzBuzz().numberDividedByThree(3));
     }
 
-    public void testFizzDividedByThree() {
+    public void testFizzNumberDividedByThree() {
         assertEquals("Fizz", new FizzBuzz().numberDividedByThree(6));
     }
 
     public void testBuzz() {
         assertEquals("Buzz", new FizzBuzz().numberDividedByFive(5));
+    }
+
+    public void testBuzzNumberDividedByFive(){
+        assertEquals("Buzz", new FizzBuzz().numberDividedByFive(10));
     }
 }
