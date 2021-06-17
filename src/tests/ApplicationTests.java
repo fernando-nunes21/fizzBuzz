e0573c191c071d0928e79070b388ae730d95609d
fernando-nunes21 <luis@ubots.com.br>
2021-06-17 04:01:46 -0300

    - @Test JUnit 4 Add. Remove TestCase Junit 3. - 91% coverage tests - Need refactor to clean more.

--- src/tests/ApplicationTests.java
+++ src/tests/ApplicationTests.java
@@ -1,42 +1,53 @@
 package tests;
 
-import junit.framework.TestCase;
 import main.FizzBuzz;
+import org.junit.Test;
 
-public class ApplicationTests extends TestCase {
+import static org.junit.Assert.assertEquals;
 
+public class ApplicationTests {
+
+    @Test
     public void inputThreeShouldReturnFizz() {
         assertEquals("Fizz", new FizzBuzz().testNumber(3));
     }
 
+    @Test
     public void inputSixShouldReturnFizz() {
         assertEquals("Fizz", new FizzBuzz().testNumber(6));
     }
 
+    @Test
     public void inputFiveShouldReturnBuzz() {
         assertEquals("Buzz", new FizzBuzz().testNumber(5));
     }
 
+    @Test
     public void inputTenShouldReturnBuzz(){
         assertEquals("Buzz", new FizzBuzz().testNumber(10));
     }
 
+    @Test
     public void inputTwoShouldReturnTwo(){
         assertEquals("2", new FizzBuzz().testNumber(2));
     }
 
+    @Test
     public void inputNumberDivisibleByThreeAndFiveShouldReturnFizzBuzz(){
         assertEquals("FizzBuzz", new FizzBuzz().testNumber(15));
     }
 
+    @Test
     public void inputNumberContainsThreeShouldReturnFizz(){
         assertEquals("Fizz", new FizzBuzz().testNumber(83));
     }
 
+    @Test
     public void inputNumberContainsFiveShouldReturnBuzz(){
         assertEquals("Buzz", new FizzBuzz().testNumber(52));
     }
 
+    @Test
     public void inputNumberContainsFiveAndAreDivisibleByThreeShouldReturnFizzBuzz(){
         assertEquals("FizzBuzz", new FizzBuzz().testNumber(156));
     }
